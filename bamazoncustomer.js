487f1dbe9d8b5a095216917b9160b75dcc194678
bnsnchz <bsanchez4488@gmail.com>
2018-06-17 18:31:58 -0700

    pushing for now

--- bamazoncustomer.js
+++ bamazoncustomer.js
@@ -5,7 +5,7 @@ var connection = mysql.createConnection({
     host: "localhost",
     port:3306,
     user:"root",
-    password:"",
+    password:"system*",
     database:"bamazon"
 });
 
@@ -20,5 +20,37 @@ start = () =>{
         for(var i = 0; i < res.length; i++){
             console.log(res[i].id, res[i].product_name, res[i].price);
         }
-       });
+        });
+        setTimeout(() => {
+       inquirer.prompt([
+           {
+            type:"input",
+            name:"menu",
+            message:"Select the number of the product you would like to purchase: "
+            }
+    ]).then((answer1)=>{
+        inquirer.prompt([
+            {
+             type:"input",
+             name:"stock",
+             message:"How many would you like?",
+            }
+            ]).then((answer2)=>{
+                connection.query("SELECT * FROM products WHERE id=?",[answer1.menu],(err, res)=>{
+                    if(res.inventory < answer2.stock){
+                        console.log("Insufficient stock.");
+                        start();
+                    }
+                    connection.query("UPDATE products SET inventory = inventory - "+(answer2.stock)+" WHERE id=?",[answer1.menu],(err)=>{
+               if(err) throw err;
+               console.log("Thank you for your buisness!");
+               start();
+                    }) 
+                });
+
+            })
+
+        })
+},3000);
+
 }
